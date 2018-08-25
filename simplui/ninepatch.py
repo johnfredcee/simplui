902b8580a0f921d8e5362669f08d172cc2a4b93f
John Connors <johnfredcee@gmail.com>
2018-08-25 10:25:07 +0100

    Fix whitespace problem

--- simplui/ninepatch.py
+++ simplui/ninepatch.py
@@ -1,14 +1,14 @@
 # ----------------------------------------------------------------------
 # Copyright (c) 2009 Joe Wreschnig, Alex Hockner, Tristam MacDonald and others
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions 
+# modification, are permitted provided that the following conditions
 # are met:
 #
 #  * Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright 
+#  * Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in
 #    the documentation and/or other materials provided with the
 #    distribution.
@@ -46,12 +46,12 @@ http://developer.android.com/guide/topics/graphics/2d-graphics.html#nine-patch.
 __all__ = ["NinePatch"]
 
 class PixelData(object):
-    def __init__(self, image):
-        image_data = image.get_image_data()
-        self.has_alpha = 'A' in image_data.format
-        self.data = image_data.get_data("RGBA", image.width * 4)
-        self.width = image.width
-        self.height = image.height
+	def __init__(self, image):
+		image_data = image.get_image_data()
+		self.has_alpha = 'A' in image_data.format
+		self.data = image_data.get_data("RGBA", image.width * 4)
+		self.width = image.width
+		self.height = image.height
 
 	def is_black(self, x, y):
 		p = (y * self.width + x) * 4
@@ -145,26 +145,26 @@ class NinePatch(object):
 			if data.is_black(x, 0):
 				self.padding_right = self.width - x
 				break
-		
+
 		if self.padding_left == None and self.padding_right == None:
 			self.padding_left, self.padding_right = self.stretch_left, self.stretch_right
-		
+
 		for y in range(1, height - 1):
 			if data.is_black(width - 1, y):
 				self.padding_bottom = y - 1
 				break
-		
+
 		for y in range(height - 2, 0, -1):
 			if data.is_black(width - 1, y):
 				self.padding_top = self.height - y
 				break
-		
+
 		if self.padding_bottom == None and self.padding_top == None:
 			self.padding_bottom, self.padding_top = self.stretch_bottom, self.stretch_top
-		
+
 		self.padding_x = self.padding_left + self.padding_right
 		self.padding_y = self.padding_bottom + self.padding_top
-		
+
 		# Texture coordinates, in pixels
 		u1 = 1
 		v1 = 1
@@ -174,16 +174,16 @@ class NinePatch(object):
 		v3 = height - self.stretch_top - 1
 		u4 = width - 1
 		v4 = height - 1
-		
+
 		# Texture coordinates as ratio of image size (0 to 1)
 		u1, u2, u3, u4 = [s / float(width) for s in (u1, u2, u3, u4)]
 		v1, v2, v3, v4 = [s / float(height) for s in (v1, v2, v3, v4)]
-		
+
 		# Scale texture coordinates to match the tex_coords pyglet gives us
 		# (these aren't necessarily 0-1 as the texture may have been packed)
-		(tu1, tv1, _, 
-		 _, _, _, 
-		 tu2, tv2, _, 
+		(tu1, tv1, _,
+		 _, _, _,
+		 tu2, tv2, _,
 		 _, _, _) = self.texture.tex_coords
 		u_scale = tu2 - tu1
 		u_bias = tu1
@@ -191,7 +191,7 @@ class NinePatch(object):
 		v_bias = tv1
 		u1, u2, u3, u4 = [u_bias + u_scale * s for s in (u1, u2, u3, u4)]
 		v1, v2, v3, v4 = [v_bias + v_scale * s for s in (v1, v2, v3, v4)]
-		
+
 		# 2D texture coordinates, bottom-left to top-right
 		self.tex_coords = (
 			u1, v1,
@@ -253,15 +253,15 @@ class NinePatch(object):
 			x3, y4,
 			x4, y4,
 		)
-	
+
 	def draw(self, x, y, width, height):
 		width = max(width, self.width + 2)
 		height = max(height, self.height + 2)
-		
+
 		glBindTexture(self.texture.target, self.texture.id)
 		graphics.draw_indexed(16, GL_QUADS, self.indices, ('v2i', self.get_vertices(int(x), int(y), int(width), int(height))), ('t2f', self.tex_coords))
 		glBindTexture(self.texture.target, 0)
-	
+
 	def draw_around(self, x, y, width, height):
 		self.draw(
 				x - self.padding_left,
@@ -269,14 +269,14 @@ class NinePatch(object):
 				width + self.padding_left + self.padding_right,
 				height + self.padding_bottom + self.padding_top
 			)
-	
+
 	def build_vertex_list(self, batch, group):
 		return batch.add_indexed(16, GL_QUADS, graphics.TextureGroup(self.texture, group), self.indices, 'v2i', ('t2f', self.tex_coords))
-	
+
 	def update_vertex_list(self, vertex_list, x, y, width, height):
 		width = max(width, self.width + 2)
 		height = max(height, self.height + 2)
-		
+
 		vertex_list.vertices = self.get_vertices(x, y, width, height)
 		vertex_list.tex_coords = self.tex_coords
 
